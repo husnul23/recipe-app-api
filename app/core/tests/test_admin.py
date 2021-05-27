a5b779caa852a5365b09fd03cb2c47ed224cb361
Husnul Fadhilah <36095304+husnul23@users.noreply.github.com>
2021-05-27 16:00:47 +0700

    added test cases for AdminSiteTests

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,26 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+class AdminSiteTests(TestCase):
+
+    def setup(self):
+        self.client = Client()
+        self.admin_user = get_user_model().create_superuser(
+            email='admin@rocket.com', 
+            password = 'password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@rocket.com',
+            password = 'password123',
+            name = 'Test user full name'
+        )
+
+    def test_user_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
