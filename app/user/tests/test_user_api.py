ab9a9c4ce59237d1c98faa1f5ab390480021ba1a
Husnul Fadhilah <36095304+husnul23@users.noreply.github.com>
2021-06-08 09:53:29 +0700

    added endpoint for creating and authenticating users

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -6,9 +6,9 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 def create_user(**params):
-    
     return get_user_model().objects.create_user(**params)
 
 class PublicUserApiTests(TestCase):
@@ -42,7 +42,7 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         """test that the password must be more than 5 characters"""
-        payload = {'email': 'test@rocket.com', 'password': 'pw'}
+        payload = {'email': 'test@rocket.com', 'password' : 'pw'}
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
@@ -50,3 +50,35 @@ class PublicUserApiTests(TestCase):
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """test that a token is created for the user"""
+        payload = {'email': 'test@rocket.com', 'password': 'testpass'}
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credentials(self):
+        """Test that token is not created if invalid credentials are given"""
+        create_user(email='test@rocket.com', password="testpass")
+        payload = {'email': 'test@rocket.com', 'password': 'wrong'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_no_user(self):
+        """Test that token is not created if user does not exist"""
+        payload = {'email': 'test@rocket.com', 'password': 'testpass'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        
+    def test_create_token_missing_filed(self):
+        """Test that email and password are required"""
+        res = self.client.post(TOKEN_URL, {'email': 'rocket', 'password': ''})
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
